cfcda7187843aa2340161f77379aff1c71d98a64
systemonyx2025 <systemonyx2025@gmail.com>
2026-01-24 01:40:21 -0300

    Add files via upload

--- static/js/config.js
+++ static/js/config.js
@@ -1,6 +1,6 @@
 window.config = {
   GA_ID: 'G-RN4NB0DBXX',
-  META_PIXEL_ID: '1460937748732276',
+  META_PIXEL_ID: '760362110477102',
   CHECKOUT_URL: 'https://pay.cakto.com.br/33zwtrw_739399',
   FLOW_NAME: 'ebook',
   STREAM_ID: '13330867318',
